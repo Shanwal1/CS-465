1370aab13623a084d6167b5819dc5d6d142e15af
Shanwal1 <shannonwalden1@gmail.com>
2024-06-09 12:55:21 -0400

    Module 5 completed Baseline

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Define Routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
+
 var handlebars = require('hbs');
+
+//Bring in the database
+require('./app_api/models/db');
 
 var app = express();
 
@@ -17,15 +23,19 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 //register handlerbars partials
 handlebars.registerPartials(__dirname + './app_server/views/partials');
 
+app.set('view engine', 'hbs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//wire-up routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
